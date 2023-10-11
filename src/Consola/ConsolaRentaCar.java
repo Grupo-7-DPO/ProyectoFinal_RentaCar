beea4f543ab0d3a773d9f251a818b4bcd5e67b15
Ignacio Chaparro <i.chaparro@uniandes.edu.co>
2023-10-10 21:32:55 -0500

    Correcion de la consola y el model

--- src/Consola/ConsolaRentaCar.java
+++ src/Consola/ConsolaRentaCar.java
@@ -20,6 +20,7 @@ public class ConsolaRentaCar {
 	
 	
 	private static RentaCar renta_carros;
+	private static Usuario usuario_actual;
 	
 	public static void main(String[] args) {
 		
@@ -31,7 +32,7 @@ public class ConsolaRentaCar {
 	        System.out.println("Error de I/O: " + e.getMessage());
 	    }
 		System.out.println("¡¡¡Bienvenido a nuestra renta de carros!!!");
-		Usuario usuario_actual = login();
+		usuario_actual = login();
 		System.out.println("\nBienvenido " + usuario_actual.getNombre());
 		
 		if (usuario_actual.getTipo().equals("AG")) {
@@ -85,8 +86,8 @@ public class ConsolaRentaCar {
 			if (eleccion.equals("1")) {
 				String username = input("Usuario");
 				String password = input("Contraseña");
-				usuario = encontrarUsuario(username, password);
-				if (usuario.equals(null)) {
+				usuario = renta_carros.encontrarUsuario(username, password);
+				if (usuario == null) {
 					System.out.println("\nERROR: Usuario o contraseña incorrectos\n Si no tienes una cuenta creada, elige la opcion 2\n");
 				}
 				else {
@@ -106,7 +107,6 @@ public class ConsolaRentaCar {
 	}
 	
 	
-	
 	private static Usuario crearUsuarioCliente() {
 		boolean user_valido = true;
 		
@@ -115,7 +115,7 @@ public class ConsolaRentaCar {
 		String username = null;
 		while (user_valido) {
 			username = input("\nEscribe tu nombre de usuario");
-			if (encontrarUsername(username)) {
+			if (renta_carros.encontrarUsername(username)) {
 				System.out.println("\nEste nombre de usuario ya está en uso\n");
 			}
 			else {
@@ -123,115 +123,31 @@ public class ConsolaRentaCar {
 			}
 		}
 		String password = input("Escribe tu contraseña");
-		Usuario nuevo_usuario = new Usuario(nombre, tipo, username, password);
-		renta_carros.getUsuarios().add(nuevo_usuario);
+		renta_carros.crearUsuarioCliente(nombre, tipo, username, password);
 		crearCliente(nombre, username);
-		try {
-			modificarArchivoUsuario(nuevo_usuario);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
 		System.out.println("\n¡¡Cuenta creada correctamente!!\n");
+		Usuario nuevo_usuario = renta_carros.encontrarUsuario(username, password);
 		return nuevo_usuario;
 	}
 
 	private static void crearCliente(String nombre, String username) {
 		String contacto = input("\nEscribe tu numero de contacto");
-		String fecha_nacimiento = input("\nEscribe tu fecha de nacimiento");
+		String fecha_nacimiento = input("\nEscribe tu fecha de nacimiento(dd/mm/aaaa)");
 		String nacionalidad = input("\nEscribe tu nacionalidad");
 		String imagen_documento = input("\nIngresa la imagen tu documento de identidad en formato .png (nombrearchivo.png)");
 		String numero_licencia = input("\nIngresa el numero de tu licencia");
 		String pais_licencia = input("\nIngresa el pais de tu licencia");
-		String fecha_vencimiento_pase = input("\nIngresa la fecha de vencimiento de tu licencia de conduccion");
+		String fecha_vencimiento_pase = input("\nIngresa la fecha de vencimiento de tu licencia de conduccion(dd/mm/aaaa)");
 		String imagen_pase = input("\nIngresa la imagen de tu licencia en formato .png(nombrearchivo.png)");
 		String tipo_tarjeta = input("\nIngresa el tipo de tu tarjeta de credito(visa, mastercard,etc.)");
 		
-		String numero_tarjeta = input("\nIngrsa el numero de tu tarjeta de credito");
-		String fecha_vencimiento_tarjeta = input("\nIngresa la fecha de vencimiento de tu tarjeta de credito");
+		String numero_tarjeta = input("\nIngresa el numero de tu tarjeta de credito");
+		String fecha_vencimiento_tarjeta = input("\nIngresa la fecha de vencimiento de tu tarjeta de credito(mm/aaaa)");
 		
-		TarjetaCredito tarjeta = new TarjetaCredito(tipo_tarjeta, numero_tarjeta, fecha_vencimiento_tarjeta);
-		LicenciaConduccion licencia = new LicenciaConduccion(numero_licencia, pais_licencia, fecha_vencimiento_pase, imagen_pase);
-		
-		Cliente nuevo_cliente = new Cliente(nombre, contacto, fecha_nacimiento, nacionalidad, imagen_documento, licencia, tarjeta, username);
-		renta_carros.getClientes().add(nuevo_cliente);
-		
-		try {
-			modificarArchivoClientes(nuevo_cliente);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		renta_carros.crearCliente(nombre, contacto, fecha_nacimiento, nacionalidad, imagen_documento, numero_licencia, pais_licencia,
+									fecha_vencimiento_pase, imagen_pase, tipo_tarjeta, numero_tarjeta, fecha_vencimiento_tarjeta, username);
 	}
 
-	private static void modificarArchivoClientes(Cliente nuevo_cliente) throws IOException {
-		FileWriter file = new FileWriter("./data/clientes.txt", true);
-		BufferedWriter br = new BufferedWriter(file);
-		
-		String nombre = nuevo_cliente.getNombre();
-		String contacto = nuevo_cliente.getContacto();
-		String fecha_nacimiento = nuevo_cliente.getNacimiento();
-		String nacionalidad = nuevo_cliente.getNacionalidad();
-		String imagen_documento = nuevo_cliente.getImagen_cedula();
-		
-		LicenciaConduccion licencia = nuevo_cliente.getLicencia();
-		String numero_licencia = licencia.getNumeroLicencia();
-		String pais_licencia = licencia.getPaisLicencia();
-		String fecha_vencimiento_licencia = licencia.getFechaVencimiento();
-		String imagen_licencia = licencia.getImagen();
-		
-		TarjetaCredito tarjeta = nuevo_cliente.getTarjeta();
-		String tipo_tarjeta = tarjeta.getTipo();
-		String numero_tarjeta = tarjeta.getNumero();
-		String fecha_vencimiento_tarjeta = tarjeta.getFechaVencimiento();
-		
-		String username = nuevo_cliente.getUser();
-		
-		br.write("\n" + nombre + ";" + contacto + ";" + fecha_nacimiento + ";" + nacionalidad + ";" + imagen_documento + ";" + numero_licencia + ";" + imagen_documento + ";" + 
-					numero_licencia + ";" + pais_licencia + ";" + fecha_vencimiento_licencia + ";" + imagen_licencia + ";" + tipo_tarjeta + ";" + numero_tarjeta + ";" + 
-					fecha_vencimiento_tarjeta + ";" + username);
-		br.close();
-		
-	}
-
-	private static void modificarArchivoUsuario(Usuario nuevo_usuario) throws IOException { 
-		//Este metodo va a modificar ek archivo de usuarios, recibiendo como parametro el usuario nuevo
-		FileWriter file = new FileWriter("./data/usuarios.txt", true);
-		BufferedWriter br = new BufferedWriter(file);
-		
-		String nombre = nuevo_usuario.getNombre();
-		String tipo = nuevo_usuario.getTipo();
-		String username = nuevo_usuario.getUser();
-		String password = nuevo_usuario.getPassword();
-		
-		br.write("\n" + nombre + ";" + tipo + ";" + username + ";" + password);
-		br.close();
-	}
-
-	private static boolean encontrarUsername(String username_buscado) {
-		//esta funcion se encarga de encontrar si ya existe el username que entra por parametro
-		boolean se_encontro = false;
-		List<Usuario> lista = renta_carros.getUsuarios();
-		
-		for (Usuario usuario: lista) {
-			if(usuario.getUser().equals(username_buscado)) {
-				se_encontro = true;
-			}
-		}
-		
-		return se_encontro;
-	}
-	
-	private static Usuario encontrarUsuario(String username, String password) {
-		// esto busca en la lista general de usuarios y encuentra si un usuario existe o no. Si no retorna null y si si retorna el usuario
-		List<Usuario> lista = renta_carros.getUsuarios();
-		Usuario usuario_buscado = null;
-		
-		for(Usuario usuario: lista) {
-			if (usuario.getUser().equals(username) && usuario.getPassword().equals(password)) {
-				usuario_buscado = usuario;
-			}
-		}
-		return usuario_buscado;
-	}
 
 	public static String input(String mensaje)
 	{
--- src/Loader/RentaCar.java
+++ src/Loader/RentaCar.java
@@ -1,11 +1,16 @@
 package Loader;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 
 import Model.Cliente;
 import Model.Empleado;
+import Model.LicenciaConduccion;
 import Model.Sede;
+import Model.TarjetaCredito;
 import Model.Usuario;
 import Model.Vehiculo;
 
@@ -35,5 +40,99 @@ public class RentaCar {
 	
 	public List<Cliente> getClientes(){
 		return this.clientes;
+	}
+	
+	public Usuario encontrarUsuario(String username, String password) {
+		// esto busca en la lista general de usuarios y encuentra si un usuario existe o no. Si no retorna null y si si retorna el usuario
+		List<Usuario> lista = this.usuarios;
+		Usuario usuario_buscado = null;
+		
+		for(Usuario usuario: lista) {
+			if (usuario.getUser().equals(username) && usuario.getPassword().equals(password)) {
+				usuario_buscado = usuario;
+			}
+		}
+		return usuario_buscado;
+	}
+	
+	public boolean encontrarUsername(String username_buscado) {
+		//esta funcion se encarga de encontrar si ya existe el username que entra por parametro
+		boolean se_encontro = false;
+		List<Usuario> lista = this.getUsuarios();
+		
+		for (Usuario usuario: lista) {
+			if(usuario.getUser().equals(username_buscado)) {
+				se_encontro = true;
+			}
+		}
+		
+		return se_encontro;
+	}
+	public void crearCliente(String nombre, String contacto, String fecha_nacimiento, String nacionalidad, String imagen_documento, String numero_licencia, String pais_licencia,
+			String fecha_vencimiento_pase, String imagen_pase, String tipo_tarjeta, String numero_tarjeta, String fecha_vencimiento_tarjeta, String username) {
+		TarjetaCredito tarjeta = new TarjetaCredito(tipo_tarjeta, numero_tarjeta, fecha_vencimiento_tarjeta);
+		LicenciaConduccion licencia = new LicenciaConduccion(numero_licencia, pais_licencia, fecha_vencimiento_pase, imagen_pase);
+		
+		Cliente nuevo_cliente = new Cliente(nombre, contacto, fecha_nacimiento, nacionalidad, imagen_documento, licencia, tarjeta, username);
+		this.clientes.add(nuevo_cliente);
+		try {
+			modificarArchivoClientes(nuevo_cliente);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void modificarArchivoClientes(Cliente nuevo_cliente) throws IOException {
+		FileWriter file = new FileWriter("./data/clientes.txt", true);
+		BufferedWriter br = new BufferedWriter(file);
+			
+		String nombre = nuevo_cliente.getNombre();
+		String contacto = nuevo_cliente.getContacto();
+		String fecha_nacimiento = nuevo_cliente.getNacimiento();
+		String nacionalidad = nuevo_cliente.getNacionalidad();
+		String imagen_documento = nuevo_cliente.getImagen_cedula();
+			
+		LicenciaConduccion licencia = nuevo_cliente.getLicencia();
+		String numero_licencia = licencia.getNumeroLicencia();
+		String pais_licencia = licencia.getPaisLicencia();
+		String fecha_vencimiento_licencia = licencia.getFechaVencimiento();
+		String imagen_licencia = licencia.getImagen();
+			
+		TarjetaCredito tarjeta = nuevo_cliente.getTarjeta();
+		String tipo_tarjeta = tarjeta.getTipo();
+		String numero_tarjeta = tarjeta.getNumero();
+		String fecha_vencimiento_tarjeta = tarjeta.getFechaVencimiento();
+			
+		String username = nuevo_cliente.getUser();
+			
+		br.write("\n" + nombre + ";" + contacto + ";" + fecha_nacimiento + ";" + nacionalidad + ";" + imagen_documento + ";" + numero_licencia + ";" + imagen_documento + ";" + 
+						numero_licencia + ";" + pais_licencia + ";" + fecha_vencimiento_licencia + ";" + imagen_licencia + ";" + tipo_tarjeta + ";" + numero_tarjeta + ";" + 
+						fecha_vencimiento_tarjeta + ";" + username);
+		br.close();
+			
+		}
+	
+	public void crearUsuarioCliente(String nombre, String tipo, String username, String password) {
+		Usuario nuevo_usuario = new Usuario(nombre, tipo, username, password);
+		this.usuarios.add(nuevo_usuario);
+		try {
+			modificarArchivoUsuario(nuevo_usuario);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	 }
+	
+	private void modificarArchivoUsuario(Usuario nuevo_usuario) throws IOException { 
+		//Este metodo va a modificar ek archivo de usuarios, recibiendo como parametro el usuario nuevo
+		FileWriter file = new FileWriter("./data/usuarios.txt", true);
+		BufferedWriter br = new BufferedWriter(file);
+		
+		String nombre = nuevo_usuario.getNombre();
+		String tipo = nuevo_usuario.getTipo();
+		String username = nuevo_usuario.getUser();
+		String password = nuevo_usuario.getPassword();
+		
+		br.write("\n" + nombre + ";" + tipo + ";" + username + ";" + password);
+		br.close();
 	}
 }
